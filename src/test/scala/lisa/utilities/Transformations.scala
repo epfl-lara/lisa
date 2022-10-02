8214bb936332791280fd78e9eee6b2ed08bb8e38
= <=>
2022-10-02 19:29:12 +0200

    comments

---
+++ src/test/scala/lisa/utilities/Transformations.scala
@@ -0,0 +1,6 @@
+package lisa.utilities
+import lisa.test.ProofCheckerSuite
+import lisa.utils.Printer
+class Transformations extends ProofCheckerSuite {
+    import lisa.proven.SetTheoryLibrary.*
+}
