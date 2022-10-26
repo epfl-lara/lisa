8602a28363da8d364828bc4abc92cf634cabbebe
SimonGuilloud <sim-guilloud@bluewin.ch>
2022-10-26 15:07:27 +0200

    Remove blank line

--- lisa-kernel/src/main/scala/lisa/kernel/fol/FormulaLabelDefinitions.scala
+++ lisa-kernel/src/main/scala/lisa/kernel/fol/FormulaLabelDefinitions.scala
@@ -17,7 +17,6 @@ private[fol] trait FormulaLabelDefinitions extends CommonDefinitions {
    * In logical terms it is a predicate symbol.
    */
   sealed trait PredicateLabel extends FormulaLabel {
-
     require(arity < MaxArity && arity >= 0)
   }
 
