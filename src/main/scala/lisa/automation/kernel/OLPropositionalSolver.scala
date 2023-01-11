8bfd69a2c1f0a6a4c12088f4f38cfe9eb37445f1
SimonGuilloud <simon.guilloud@bluewin.ch>
2023-01-11 17:58:41 +0100

    New OL based propositional solver seems fully working. Need more test cases.

--- src/main/scala/lisa/automation/kernel/OLPropositionalSolver.scala
+++ src/main/scala/lisa/automation/kernel/OLPropositionalSolver.scala
@@ -4,7 +4,8 @@ import lisa.kernel.proof.SequentCalculus as LK
 import lisa.kernel.proof.SequentCalculus.*
 import lisa.kernel.proof.SCProof
 import lisa.kernel.fol.FOL.*
-import lisa.utils.Helpers.{given, *}
+import lisa.utils.Helpers.{*, given}
+import lisa.utils.tactics.SimpleDeducedSteps.Restate
 
 object OLPropositionalSolver {
   
@@ -35,12 +36,12 @@ object OLPropositionalSolver {
   }
 
   private class NoProofFoundException extends Exception
-  def solveSequent(s:Sequent): Option[List[SCProofStep]] = {
+  def solveSequent(s:Sequent): Option[SCProof] = {
     val augSeq = augmentSequent(s)
     val MaRvIn = VariableFormulaLabel(freshId(augSeq.formula.schematicFormulaLabels.map(_.id), "MaRvIn")) //arbitrary name that is unlikely to already exist in the formula
 
     try {
-      Some(solveAugSequent(augSeq, 0)(using MaRvIn ).reverse)
+      Some(SCProof(solveAugSequent(augSeq, 0)(using MaRvIn ).reverse.toIndexedSeq))
     }
     catch case e:NoProofFoundException => None
 
@@ -48,7 +49,7 @@ object OLPropositionalSolver {
   //Given a sequent, return a proof of that sequent if on exists that only uses propositional logic rules and reflexivity of equality.
   private def solveAugSequent(s:AugSequent, offset:Int)(using MaRvIn : VariableFormulaLabel): List[SCProofStep] = {
     if (isSame(s.formula, top())) {
-      List(RewriteTrue(s.decisions._1 |- s.formula::s.decisions._2))
+      List(RewriteTrue(s.decisions._1++s.decisions._2.map((f:Formula) => Neg(f)) |- s.formula))
     }
     else if (s.atoms.isEmpty) {
       throw new NoProofFoundException
@@ -56,16 +57,23 @@ object OLPropositionalSolver {
     else {
       val atom = s.atoms.head
       val remaining = s.atoms.tail
-      val optLambda = SimpleSimplifier.findSubformula(s.formula, Seq((MaRvIn, atom)))
-      if (optLambda.isEmpty) return solveAugSequent(AugSequent(s.decisions, s.formula, remaining), offset)
+      val redF = reducedForm(s.formula)
+      val optLambda = SimpleSimplifier.findSubformula(redF, Seq((MaRvIn, atom)))
+      if (optLambda.isEmpty) return solveAugSequent(AugSequent(s.decisions, redF, remaining), offset)
       val lambdaF = optLambda.get
 
-      val seq1 = AugSequent((atom::s.decisions._1, s.decisions._2), reducedForm(lambdaF(Seq(top()))), remaining)
+
+
+      val seq1 = AugSequent((atom::s.decisions._1, s.decisions._2), lambdaF(Seq(top())), remaining)
       val proof1 = solveAugSequent(seq1, offset)
-      val seq2 = AugSequent((s.decisions._1, atom::s.decisions._2), reducedForm(lambdaF(Seq(bot()))), remaining)
-      val proof2 = solveAugSequent(seq2, offset+proof1.length)
-      val newStep = Cut(s.decisions._1 |- s.formula::s.decisions._2, offset+proof1.length-1, offset+proof1.length+proof2.length-1, atom)
-      newStep::(proof2++proof1)
+      val subst1 = RightSubstIff(atom::s.decisions._1++s.decisions._2.map((f:Formula) => Neg(f)) |- redF, offset+proof1.length-1, List((atom, top())), lambdaF)
+      val seq2 = AugSequent((s.decisions._1, atom::s.decisions._2), lambdaF(Seq(bot())), remaining)
+      val proof2 = solveAugSequent(seq2, offset+proof1.length+1)
+      val subst2 = RightSubstIff(Neg(atom)::s.decisions._1++s.decisions._2.map((f:Formula) => Neg(f)) |- redF, offset+proof1.length+proof2.length-1+1, List((atom, bot())), lambdaF)
+      val red2 = Rewrite(s.decisions._1++s.decisions._2.map((f:Formula) => Neg(f)) |- (redF, atom), offset+proof1.length+proof2.length+2-1)
+      val cutStep = Cut(s.decisions._1 |- redF::s.decisions._2, offset+proof1.length+proof2.length+3-1, offset+proof1.length+1-1, atom)
+      val redStep = Rewrite(s.decisions._1 |- s.formula::s.decisions._2, offset+proof1.length+proof2.length+4-1)
+      redStep::cutStep::red2::subst2::proof2++(subst1::proof1)
 
       }
     }
