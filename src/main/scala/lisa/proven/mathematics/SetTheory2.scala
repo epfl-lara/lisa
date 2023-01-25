312280f4900ff20aba9d62bcfc3a250bd9a0c3e1
Sankalp Gambhir <sankalp.gambhir47@gmail.com>
2023-01-25 14:43:34 +0100

    relations and functions

--- src/main/scala/lisa/proven/mathematics/SetTheory2.scala
+++ src/main/scala/lisa/proven/mathematics/SetTheory2.scala
@@ -334,4 +334,11 @@ object SetTheory2 extends lisa.proven.mathematics.BasicDefs {
   show
 
   val relationRange = DEF (r) --> The(z, forall(t, in(t, z) <=> (in(t, union(union(r))) /\ exists(a, in(pair(a, t), r)))))(relationRangeUniqueness)
+
+  val relationField = DEF (r) --> (setUnion(relationDomain(r), relationRange(r)))
+
+  val functionalOver = DEF (f, x) --> (relation(f, x) /\ forall(x, in(x, relationDomain(f)) ==> existsOne(y, in(y, relationRange(f)) /\ in(pair(x, y), f))))
+
+  val functional = DEF (f) --> functionalOver(f, relationDomain(f))
+
 }
