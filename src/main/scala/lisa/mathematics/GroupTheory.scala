dd4b657f20a101d101d41363a9de1c2a1e65494d
dhalilov <dario.halilovic@epfl.ch>
2023-03-24 19:21:41 +0100

    Add minor comments tweaks

--- src/main/scala/lisa/mathematics/GroupTheory.scala
+++ src/main/scala/lisa/mathematics/GroupTheory.scala
@@ -11,12 +11,25 @@ import lisa.mathematics.SetTheory.*
  * Book : [[https://link.springer.com/book/10.1007/978-1-4684-9234-7]]
  */
 object GroupTheory extends lisa.Main {
+  // Groups
+  private val G = variable
 
-  private val G, * = variable
+  // Group laws
+  private val * = variable
+
+  // Group elements
   private val x, y, z = variable
   private val u, v, w = variable
+
+  // Identity elements
   private val e, f = variable
+
+  // Predicates
   private val P, Q = predicate(1)
+
+  //
+  // 0. Notation
+  //
 
   /**
    * Bounded quantifiers --- These express the usual `∀x ∈ G ϕ` and `∃x ∈ G ϕ`, for some variables (sets) `x` and `G`, which
@@ -117,15 +130,18 @@ object GroupTheory extends lisa.Main {
 
         have(thesis) by Cases(case1, case2)
       }
-      show
 
       The(u, completeDef)(completeUniquenessTheorem)
     }
   }
 
+  //
+  // 1. Basic definitions and results
+  //
+
   /**
    * Binary relation --- `*` is a binary relation on `G` if it associates to each pair of elements of `G`
-   * a unique element in `G`. In other words, `*` is a function `G x G -> G`.
+   * a unique element in `G`. In other words, `*` is a function `G × G -> G`.
    */
   val binaryRelation = DEF(G, *) --> functionFrom(*, cartesianProduct(G, G), G)
 
@@ -135,18 +151,18 @@ object GroupTheory extends lisa.Main {
   val op = DEF(x, *, y) --> app(*, pair(x, y))
 
   /**
-   * Associativity --- `*` is associative (in G) if `(x * y) * z = x * (y * z)` for all `x, y, z` in G.
+   * Associativity --- `*` is associative (in `G`) if `(x * y) * z = x * (y * z)` for all `x, y, z` in `G`.
    */
   val associativity = DEF(G, *) -->
     ∀(x, G, ∀(y, G, ∀(z, G, op(op(x, *, y), *, z) === op(x, *, op(y, *, z)))))
 
   /**
-   * Neutral element --- We say that an element `e` in G is neutral if `e * x = x * e = x` for all `x` in G.
+   * Neutral element --- We say that an element `e` in `G` is neutral if `e * x = x * e = x` for all `x` in `G`.
    */
   val isNeutral = DEF(e, G, *) --> (in(e, G) /\ ∀(x, G, (op(e, *, x) === x) /\ (op(x, *, e) === x)))
 
   /**
-   * Identity existence --- There exists a neutral element `e` in G.
+   * Identity existence --- There exists a neutral element `e` in `G`.
    */
   val identityExistence = DEF(G, *) --> ∃(e, isNeutral(e, G, *))
 
@@ -226,14 +242,13 @@ object GroupTheory extends lisa.Main {
 
     // Assume y and z are inverses of x.
     // We prove the following chain of equalities:
-    //   z =1= (yx)z =2= y(xz) =3= y
+    //   z === (yx)z === y(xz) === y
     // where equalities come from
     //   1. Left neutrality of yx
     //   2. Associativity
     //   3. Right neutrality of xz
     val uniqueness = have((group(G, *), in(x, G), isInverse(y, x, G, *), isInverse(z, x, G, *)) |- (y === z)) subproof {
       val inverseMembership = have(isInverse(y, x, G, *) |- in(y, G)) by Tautology.from(isInverse.definition)
-
 
       assume(group(G, *))
       assume(in(x, G))
@@ -298,7 +313,7 @@ object GroupTheory extends lisa.Main {
       )
     }
     
-    have(thesis) by ExistenceAndUniqueness.withParameters(isInverse(y, x, G, *), y, z)(existence, uniqueness)
+    have(thesis) by ExistenceAndUniqueness(isInverse(y, x, G, *))(existence, uniqueness)
   }
 
   /**
@@ -323,7 +338,6 @@ object GroupTheory extends lisa.Main {
 
       have(thesis) by Tautology.from(definition, isInverse.definition of (y -> x, x -> y))
     }
-    thenHave((group(G, *), in(x, G)) |- isInverse(y, x, G, *) ==> isInverse(x, y, G, *)) by Restate
     thenHave(group(G, *) |- in(x, G) ==> (isInverse(y, x, G, *) ==> isInverse(x, y, G, *))) by Restate
     thenHave(thesis) by RightForall
   }
