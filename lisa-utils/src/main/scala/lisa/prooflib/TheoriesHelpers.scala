be66d514f317f7ef12c9aed9e816ba271cea551a
Sankalp Gambhir <sankalp.gambhir47@gmail.com>
2023-04-27 14:21:09 +0200

    Fix: make show check for sorry-using theorems

--- lisa-utils/src/main/scala/lisa/prooflib/TheoriesHelpers.scala
+++ lisa-utils/src/main/scala/lisa/prooflib/TheoriesHelpers.scala
@@ -22,14 +22,20 @@ object TheoriesHelpers {
   export lisa.utils.KernelHelpers.{_, given}
 
   extension (just: RunningTheory#Justification) {
-
     /**
      * Outputs, with an implicit om.output function, a readable representation of the Axiom, Theorem or Definition.
      */
     def show(using om: OutputManager): just.type = {
-      om.output(just.repr, Console.GREEN)
+      just match {
+        case j: RunningTheory#Theorem =>
+          if (j.withSorry) om.output(j.repr, Console.YELLOW)
+          else om.output(j.repr, Console.GREEN)
+        case j: RunningTheory#FunctionDefinition =>
+          if (j.withSorry) om.output(j.repr, Console.YELLOW)
+          else om.output(j.repr, Console.GREEN)
+        case _ => om.output(just.repr, Console.GREEN)
+      }
       just
-
     }
   }
 
