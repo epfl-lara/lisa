be1bfa9a48f724c08a03a14ea06197ccef8bcaa2
SimonGuilloud <sim-guilloud@bluewin.ch>
2023-10-15 22:03:28 +0200

    Continued work on princess integration

--- build.sbt
+++ build.sbt
@@ -58,7 +58,7 @@ def githubProject(repo: String, commitHash: String) = RootProject(uri(s"$repo#$c
 lazy val scallion = githubProject("https://github.com/sankalpgambhir/scallion.git", "6434e21bd08872cf547c8f0efb67c963bfdf4190")
 lazy val silex = githubProject("https://github.com/epfl-lara/silex.git", "fc07a8670a5fa8ea2dd5649a00424710274a5d18")
 // lazy val princess = RootProject(file("../princess")) // If you have a local copy of Princess and would like to do some changes
-lazy val princess = githubProject("https://github.com/uuverifiers/princess.git", "93cbff11d7b02903e532c7b64207bc12f19b79c7")
+//lazy val princess = githubProject("https://github.com/uuverifiers/princess.git", "93cbff11d7b02903e532c7b64207bc12f19b79c7")
 
 lazy val root = Project(
     id = "lisa",
--- lisa-examples/src/main/scala/Princess.scala
+++ lisa-examples/src/main/scala/Princess.scala
@@ -16,28 +16,32 @@ object Princess {
 
             val a, b, c = createBooleanVariable
             val P = createRelation("P", 1)
-            val Q = createRelation("Q", 1)
+            val Q = createRelation("Q", 2)
 
             setConstructProofs(true)
 
-            !!(!(a & (b | c)) & ((a & b) | (a & c)))
+            //!!(!(a & (b | c)) & ((a & b) | (a & c)))
 
-            println(ex(y => all((IVariable(0) === y) <=> P(IVariable(0))) & Q(y)))
+            //println(ex(y => all((IVariable(0) === y) <=> P(IVariable(0))) & Q(y, IVariable(0))))
 
 
             val x = K.variable
             val y = K.variable
             val z = K.variable
-            val P = K.predicate(1)
-            val Q = K.predicate(2)
-
-            PrincessEnvironment(p).formulaL2P_Map(K.forall())
-            
-
-            //println(???)
+            val P1 = K.predicate(1)
+            val Q1 = K.predicate(2)
+            val form = {
+                import lisa.utils.K.{given, *}
+                val unders = Example.fixedPointDoubleApplication.goal.underlying
+                PrincessEnvironment(p).formulaLisa2Princess((unders.left.head ==> forall(x, unders.right.head)))
+            }
+            val form2 = all(P(IVariable(0)) & ex( or(List(Q(IVariable(1), IVariable(0)), IVariable(1) === IVariable(0))) ))
+            println(form)
+            println(form2)
+            ??(form)
+            println(???)
             //println(getCertificate)
-            // certificateAsString(partNames: Map[Int, PartName], format: parameters.Param.InputFormat.Value): String 
-            //println(certificateAsString(Map.empty, ap.parameters.Param.InputFormat.Princess))
+            println(certificateAsString(Map.empty, ap.parameters.Param.InputFormat.Princess))
 
             //println(getInterpolants(List(Set(1, 3), Set(2))))
 
@@ -51,8 +55,8 @@ object Princess {
         import scala.collection.mutable.HashMap
 
 
-        val termL2P_Map : HashMap[K.Term, ITerm] = HashMap.empty
-        val termP2L_Map : HashMap[ITerm, K.Term] = HashMap.empty
+        //val termL2P_Map : HashMap[K.Term, ITerm] = HashMap.empty
+        //val termP2L_Map : HashMap[ITerm, K.Term] = HashMap.empty
 
         val functionL2P_Map : HashMap[K.TermLabel, IFunction] = HashMap.empty
         val functionP2L_Map : HashMap[IFunction, K.TermLabel] = HashMap.empty
@@ -84,18 +88,21 @@ object Princess {
                     newf
         }
 
-        def termLisa2Princess(term: K.Term): ITerm = {
-            termL2P_Map.get(term) match
+        /**
+          * Create and memoize terms between a LISA term and its Princess counterpart
+          */ 
+        def termLisa2Princess(term: K.Term, depth:Int, vMap:Map[K.Identifier, Int]): ITerm = {
+            /*termL2P_Map.get(term) match
                 case Some(value) => return value
-                case None => ()
+                case None => ()*/
             val K.Term(label, args) = term
             val output: ITerm = label match
                 case f : (K.ConstantFunctionLabel | K.SchematicFunctionLabel) => 
-                    functionLisa2Princess(f)(args.map(termLisa2Princess)*)
-                case K.VariableLabel(id) => ???
+                    functionLisa2Princess(f)(args.map(termLisa2Princess(_, depth, vMap))*)
+                case K.VariableLabel(id) => IVariable(depth-vMap(id))
             
-            termL2P_Map.update(term, output)
-            termP2L_Map.update(output, term)
+            //termL2P_Map.update(term, output)
+            //termP2L_Map.update(output, term)
             output
         }
 
@@ -104,6 +111,9 @@ object Princess {
         val formulaL2P_Map : HashMap[K.Formula, IFormula] = HashMap.empty
         val formulaP2L_Map : HashMap[IFormula, K.Formula] = HashMap.empty
 
+        /**
+          * Create and memoize formulas between a LISA formula and its Princess counterpart
+          */ 
         def formulaLisa2Princess(formula: K.Formula, depth:Int = 0, vMap:Map[K.Identifier, Int] = Map.empty): IFormula = {
 
             formulaL2P_Map.get(formula) match
@@ -112,12 +122,12 @@ object Princess {
             
             val output = formula match
                 case K.BinderFormula(label, bound, inner) => label match
-                    case K.Exists => ex(formulaLisa2Princess(inner, depth+1, vMap + (bound.id -> (depth))))
+                    case K.Exists => ex(formulaLisa2Princess(inner, depth+1, vMap + (bound.id -> (depth+1))))
                     case K.ExistsOne => 
                         ex(all(
-                            (IVariable(0) === IVariable(1)) <=> formulaLisa2Princess(inner, depth+2, vMap + (bound.id -> (depth+1)))
+                            (IVariable(0) === IVariable(1)) <=> formulaLisa2Princess(inner, depth+2, vMap + (bound.id -> (depth+2)))
                         ))
-                    case K.Forall => all(formulaLisa2Princess(inner, depth+1, vMap + (bound.id -> (depth)))) 
+                    case K.Forall => all(formulaLisa2Princess(inner, depth+1, vMap + (bound.id -> (depth+1)))) 
 
                 case K.ConnectorFormula(label, args) => label match
                     case K.SchematicConnectorLabel(id, arity) =>  ???
@@ -131,7 +141,9 @@ object Princess {
                     case K.Neg => !formulaLisa2Princess(args.head, depth, vMap)
                     case K.Or => or(args.map(formulaLisa2Princess(_, depth, vMap)))
                 
-                case K.PredicateFormula(label, args) => ???
+                case K.PredicateFormula(label, args) => 
+                    if (label == K.equality) IEquation(termLisa2Princess(args.head, depth, vMap), termLisa2Princess(args(1), depth, vMap))
+                    else relationLisa2Princess(label)(args.map(termLisa2Princess(_, depth, vMap))*)
             formulaL2P_Map.update(formula, output)
             formulaP2L_Map.update(output, formula)
             output
