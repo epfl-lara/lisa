4c5e05b4d276ff3ab79b3e4eef9102ae8c08c617
Simon Guilloud <simon.guilloud@bluewin.ch>
2024-04-09 11:39:08 +0200

    Implement egraphs for terms, and partially for formulas. Tested egraphs with terms. Need to do proofs, and formulas

---
+++ lisa-sets/src/main/scala/lisa/automation/Congruence.scala
@@ -0,0 +1,339 @@
+package lisa.automation
+import lisa.fol.FOL.*
+
+object Congruence {
+  
+}
+
+
+class UnionFind[T]  {
+  // parent of each element, leading to its root. Uses path compression
+  val parent = scala.collection.mutable.Map[T, T]()
+  // original parent of each element, leading to its root. Does not use path compression. Used for explain.
+  val realParent = scala.collection.mutable.Map[T, (T, ((T, T), Boolean, Int))]()
+  //keep track of the rank (i.e. number of elements bellow it) of each element. Necessary to optimize union.
+  val rank = scala.collection.mutable.Map[T, Int]()
+  //tracks order of ancientness of unions.
+  var unionCounter = 0
+
+  /**
+    * add a new element to the union-find.
+    */
+  def add(x: T): Unit = {
+    parent(x) = x
+    realParent(x) = (x, ((x, x), true, 0))
+    rank(x) = 0
+  }
+
+  /**
+    *
+    * @param x the element whose parent we want to find
+    * @return the root of x
+    */
+  def find(x: T): T = {
+    if parent(x) == x then
+      x
+    else
+      var root = x
+      while parent(root) != root do
+        root = parent(root)
+      var y = x
+      while parent(y) != root do
+        parent(y) = root
+        y = parent(y)
+      root
+  }
+
+  /**
+    * Merges the classes of x and y
+    */
+  def union(x: T, y: T): Unit = {
+    unionCounter += 1
+    val xRoot = find(x)
+    val yRoot = find(y)
+    if (xRoot == yRoot) return
+    if (rank(xRoot) < rank(yRoot)) {
+      parent(xRoot) = yRoot
+      realParent(xRoot) = (yRoot, ((x, y), true, unionCounter))
+    } else if (rank(xRoot) > rank(yRoot)) {
+      parent(yRoot) = xRoot
+      realParent(yRoot) = (xRoot, ((x, y), false, unionCounter))
+    } else {
+      parent(yRoot) = xRoot
+      realParent(yRoot) = (xRoot, ((x, y), false, unionCounter))
+      rank(xRoot) = rank(xRoot) + 1
+    }
+  }
+
+  private def getPathToRoot(x: T): List[T] = {
+    if x == find(x) then
+      List(x)
+    else
+      val next = realParent(x)
+      x :: getPathToRoot(next._1)
+
+  }
+
+  private def getExplanationFromTo(x:T, c: T): List[(T, ((T, T), Boolean, Int))] = {
+    if x == c then
+      List()
+    else
+      val next = realParent(x)
+      next :: getExplanationFromTo(next._1, c)}
+
+  private def lowestCommonAncestor(x: T, y: T): Option[T] = {
+    val pathX = getPathToRoot(x)
+    val pathY = getPathToRoot(y)
+    pathX.find(pathY.contains)
+  }
+
+  /**
+    * Returns a path from x to y made of pairs of elements (u, v)
+    * such that union(u, v) was called.
+    */
+  def explain(x:T, y:T): Option[List[(T, T)]]= {
+
+    if (x == y) then return Some(List())
+    val lca = lowestCommonAncestor(x, y)
+    lca match
+      case None => None
+      case Some(lca) =>
+        var max :((T, T), Boolean, Int) = ((x, x), true, 0)
+        var itX = x
+        while itX != lca do
+          val (next, ((u1, u2), b, c)) = realParent(itX)
+          if c > max._3 then
+            max = ((u1, u2), b, c)
+          itX = next
+
+        var itY = y
+        while itY != lca do
+          val (next, ((u1, u2), b, c)) = realParent(itY)
+          if c > max._3 then
+            max = ((u1, u2), !b, c)
+          itY = next
+
+        val u1 = max._1._1
+        val u2 = max._1._2
+        if max._2 then
+          Some(explain(x, u1).get ++ List((u1, u2)) ++ explain(u2, y).get)
+        else
+          Some(explain(x, u2).get ++ List((u1, u2)) ++ explain(u1, y).get)
+  }
+
+
+  /**
+    * Returns the set of all roots of all classes
+    */
+  def getClasses: Set[T] = parent.keys.map(find).toSet
+
+  /**
+    * Add all elements in the collection to the union-find
+    */
+  def addAll(xs: Iterable[T]): Unit = xs.foreach(add)
+
+}
+
+
+///////////////////////////////
+///////// E-graph /////////////
+///////////////////////////////
+
+import scala.collection.mutable
+
+class EGraphTerms() {
+
+  type ENode = Term | Formula
+
+
+
+  val termMap = mutable.Map[Term, Set[Term]]()
+  val termParents = mutable.Map[Term, mutable.Set[AppliedFunction | AppliedPredicate]]()
+  var termWorklist = List[Term]()
+  val termUF = new UnionFind[Term]()
+
+
+
+
+  val formulaMap = mutable.Map[Formula, Set[Formula]]()
+  val formulaParents = mutable.Map[Formula, mutable.Set[AppliedConnector]]()
+  var formulaWorklist = List[AppliedConnector | AppliedPredicate]()
+  val formulaUF = new UnionFind[Formula]()
+
+
+
+
+  trait TermStep
+  case class TermExternal(between: (Term, Term)) extends TermStep
+  case class TermCongruence(between: (Term, Term)) extends TermStep
+
+  trait FormulaStep
+  case class FormulaExternal(between: (Formula, Formula)) extends FormulaStep
+  case class FormulaCongruence(between: (Formula, Formula)) extends FormulaStep
+
+  val termProofMap = mutable.Map[(Term, Term), TermStep]()
+  val formulaProofMap = mutable.Map[(Formula, Formula), FormulaStep]()
+
+  def explain(id1: Term, id2: Term): Option[List[TermStep]] = {
+    val steps = termUF.explain(id1, id2)
+    steps.map(_.foldLeft((id1, List[TermStep]())) {
+      case ((prev, acc), step) =>
+      termProofMap(step) match
+        case s @ TermExternal((l, r)) => 
+          if l == prev then
+            (r, s :: acc)
+          else if r == prev then
+            (l, TermExternal(r, l) :: acc)
+          else throw new Exception("Invalid proof recovered: It is not a chain")
+        case s @ TermCongruence((l, r)) => 
+          if l == prev then
+            (r, s :: acc)
+          else if r == prev then
+            (l, TermCongruence(r, l) :: acc)
+          else throw new Exception("Invalid proof recovered: It is not a chain")
+
+    }._2.reverse)
+  }
+
+  def explain(id1: Formula, id2: Formula): Option[List[FormulaStep]] = {
+    val steps = formulaUF.explain(id1, id2)
+    steps.map(_.foldLeft((id1, List[FormulaStep]())) {
+      case ((prev, acc), step) =>
+      formulaProofMap(step) match
+        case s @ FormulaExternal((l, r)) => 
+          if l == prev then
+            (r, s :: acc)
+          else if r == prev then
+            (l, FormulaExternal(r, l) :: acc)
+          else throw new Exception("Invalid proof recovered: It is not a chain")
+        case s @ FormulaCongruence((l, r)) => 
+          if l == prev then
+            (r, s :: acc)
+          else if r == prev then
+            (l, FormulaCongruence(r, l) :: acc)
+          else throw new Exception("Invalid proof recovered: It is not a chain")
+
+    }._2.reverse)
+  }
+
+
+  def makeSingletonEClass(node:Term): Term = {
+    termUF.add(node)
+    termMap(node) = Set(node)
+    termParents(node) = mutable.Set()
+    node
+  }
+  def makeSingletonEClass(node:Formula): Formula = {
+    formulaUF.add(node)
+    formulaMap(node) = Set(node)
+    formulaParents(node) = mutable.Set()
+    node
+  }
+
+  def classOf(id: Term): Set[Term] = termMap(id)
+  def classOf(id: Formula): Set[Formula] = formulaMap(id)
+
+  def idEq(id1: Term, id2: Term): Boolean = termUF.find(id1) == termUF.find(id2)
+  def idEq(id1: Formula, id2: Formula): Boolean = formulaUF.find(id1) == formulaUF.find(id2)
+
+  def canonicalize(node: Term): Term = node match
+    case AppliedFunction(label, args) => 
+      AppliedFunction(label, args.map(termUF.find.asInstanceOf))
+    case _ => node
+  
+    
+  def canonicalize(node: Formula): Formula = {
+    node match
+      case AppliedPredicate(label, args) => AppliedPredicate(label, args.map(termUF.find))
+      case AppliedConnector(label, args) => AppliedConnector(label, args.map(formulaUF.find))
+      case node => node
+  }
+
+  def add(node: Term): Term =
+    if termMap.contains(node) then return node
+    makeSingletonEClass(node)
+    node match
+      case node @ AppliedFunction(_, args) => 
+        args.foreach(child => 
+          add(child)
+          termParents(child).add(node)
+        )
+        node
+      case _ => node
+  
+  def add(node: Formula): Formula =
+    if formulaMap.contains(node) then return node
+    makeSingletonEClass(node)
+    node match
+      case node @ AppliedPredicate(_, args) => 
+        args.foreach(child => 
+          add(child)
+          termParents(child).add(node)
+        )
+        node
+      case node @ AppliedConnector(_, args) =>
+        args.foreach(child => 
+          add(child)
+          formulaParents(child).add(node)
+        )
+        node
+      case _ => node
+    
+
+
+  def merge(id1: Term, id2: Term): Unit = {
+    mergeWithStep(id1, id2, TermExternal((id1, id2)))
+  }
+  def merge(id1: Formula, id2: Formula): Unit = {
+    //mergeWithStep(id1, id2, FormulaExternal((id1, id2)))
+    ???
+  }
+
+  protected def mergeWithStep(id1: Term, id2: Term, step: TermStep): Unit = {
+    if termUF.find(id1) == termUF.find(id2) then ()
+    else
+      termProofMap((id1, id2)) = step
+      val newSet = termMap(termUF.find(id1)) ++ termMap(termUF.find(id2))
+      val newparents = termParents(termUF.find(id1)) ++ termParents(termUF.find(id2))
+      termUF.union(id1, id2)
+      val newId1 = termUF.find(id1)
+      val newId2 = termUF.find(id2)
+      termMap(newId1) = newSet
+      termMap(newId2) = newSet
+      termParents(newId1) = newparents
+      termParents(newId2) = newparents
+      val id = termUF.find(id2)
+      termWorklist = id :: termWorklist
+      val cause = (id1, id2)
+      val termSeen = mutable.Map[Term, AppliedFunction]()
+      val formulaSeen = mutable.Map[Formula, Formula]()
+      newparents.foreach { 
+        case pTerm: AppliedFunction =>
+          val canonicalPTerm = canonicalize(pTerm) 
+          if termSeen.contains(canonicalPTerm) then 
+            val qTerm = termSeen(canonicalPTerm)
+
+            Some((pTerm, qTerm, cause))
+            mergeWithStep(pTerm, qTerm, TermCongruence((pTerm, qTerm)))
+          else
+            termSeen(canonicalPTerm) = pTerm
+        case pFormula: AppliedPredicate =>
+          val canonicalPFormula = canonicalize(pFormula) 
+          if formulaSeen.contains(canonicalPFormula) then 
+            val qFormula = formulaSeen(canonicalPFormula)
+
+            Some((pFormula, qFormula, cause))
+            mergeWithStep(pFormula, qFormula, FormulaCongruence((pFormula, qFormula)))
+          else
+            formulaSeen(canonicalPFormula) = pFormula
+      }
+      termParents(id) = termSeen.values.to(mutable.Set)
+  }
+
+  protected def mergeWithStep(id1: Formula, id2: Formula, step: FormulaStep): Unit = ???
+
+
+  //def prove(id1: ENode, id2:ENode, base: SC.Sequent, stepString: String, thmName: String = "Congruence Proof"): Option[LVL2.LVL2Proof] = ???
+
+
+}
---
+++ lisa-sets/src/test/scala/lisa/automation/CongruenceTest.scala
@@ -0,0 +1,453 @@
+package lisa.automation
+import lisa.fol.FOL.{*, given}
+import lisa.automation.Congruence.*
+import org.scalatest.funsuite.AnyFunSuite
+
+
+class CongruenceTest extends AnyFunSuite {
+  val a = variable
+  val b = variable
+  val c = variable
+  val d = variable
+  val e = variable
+  val f = variable
+  val g = variable
+  val h = variable
+  val i = variable
+  val j = variable
+  val k = variable
+  val l = variable
+  val m = variable
+  val n = variable
+  val o = variable
+
+  val x = variable
+
+  val F = function[1]
+
+  val one = variable
+  val two = variable
+  val * = ConstantFunctionLabel.infix("*", 2)
+  val << = ConstantFunctionLabel.infix("<<", 2)
+  val / = ConstantFunctionLabel.infix("/", 2)
+
+
+  test("3 elements no congruence egraph test") {
+    val egraph = new EGraphTerms()
+    egraph.add(a)
+    egraph.add(b)
+    egraph.add(c)
+    egraph.merge(a, b)
+    assert(egraph.idEq(a, b))
+    assert(!egraph.idEq(a, c))
+
+  }
+
+  test("8 elements no congruence egraph test") {
+    val egraph = new EGraphTerms()
+    egraph.add(a)
+    egraph.add(b)
+    egraph.add(c)
+    egraph.add(d)
+    egraph.add(e)
+    egraph.add(f)
+    egraph.add(g)
+    egraph.add(h)
+    egraph.merge(a, b)
+    egraph.merge(c, d)
+    egraph.merge(e, f)
+    egraph.merge(g, h)
+    egraph.merge(a, c)
+    egraph.merge(f, h)
+    egraph.merge(a, f)
+    assert(egraph.idEq(a, h))
+
+  }
+
+  test("15 elements no congruence egraph test") {
+    val egraph = new EGraphTerms()
+    egraph.add(a)
+    egraph.add(b)
+    egraph.add(c)
+    egraph.add(d)
+    egraph.add(e)
+    egraph.add(f)
+    egraph.add(g)
+    egraph.add(h)
+    egraph.add(i)
+    egraph.add(j)
+    egraph.add(k)
+    egraph.add(l)
+    egraph.add(m)
+    egraph.add(n)
+    egraph.add(o)
+    egraph.merge(a, c)
+    egraph.merge(e, f)
+    egraph.merge(i, k)
+    egraph.merge(m, n)
+    egraph.merge(a, b)
+    egraph.merge(o, m)
+    egraph.merge(i, m)
+    egraph.merge(g, h)
+    egraph.merge(l, k)
+    egraph.merge(c, d)
+    egraph.merge(a, e)
+    egraph.merge(a, i)
+    egraph.merge(g, e)
+    egraph.merge(i, j)
+    assert(egraph.idEq(a, o))
+
+  }
+
+  test("15 elements no congruence egraph test with redundant merges") {
+    val egraph = new EGraphTerms()
+    egraph.add(a)
+    egraph.add(b)
+    egraph.add(c)
+    egraph.add(d)
+    egraph.add(e)
+    egraph.add(f)
+    egraph.add(g)
+    egraph.add(h)
+    egraph.add(i)
+    egraph.add(j)
+    egraph.add(k)
+    egraph.add(l)
+    egraph.add(m)
+    egraph.add(n)
+    egraph.add(o)
+    egraph.merge(a, c)
+    egraph.merge(e, f)
+    egraph.merge(i, k)
+    egraph.merge(m, n)
+    egraph.merge(a, b)
+    egraph.merge(o, m)
+    egraph.merge(i, m)
+    egraph.merge(g, h)
+    egraph.merge(l, k)
+    egraph.merge(b, c)
+    egraph.merge(f, e)
+    egraph.merge(o, i)
+    egraph.merge(g, e)
+    egraph.merge(i, j)
+
+    assert(egraph.idEq(b, c))
+    assert(egraph.idEq(f, h))
+    assert(egraph.idEq(i, o))
+    assert(!egraph.idEq(a, d))
+    assert(!egraph.idEq(b, g))
+    assert(!egraph.idEq(f, i))
+    assert(!egraph.idEq(n, c))
+    assert(egraph.termUF.getClasses.size == 4)
+
+  }
+
+  test("4 elements withcongruence egraph test") {
+    val egraph = new EGraphTerms()
+    egraph.add(F(a))
+    egraph.add(F(b))
+    egraph.merge(a, b)
+    assert(egraph.idEq(a, b))
+    assert(egraph.idEq(F(a), F(b)))
+    assert(!egraph.idEq(a, F(a)))
+    assert(!egraph.idEq(a, F(b)))
+    assert(!egraph.idEq(b, F(a)))
+    assert(!egraph.idEq(b, F(b)))
+
+  }
+
+
+
+  test("divide-mult-shift by 2 egraph test") {
+
+    val egraph = new EGraphTerms()
+    egraph.add(one)
+    egraph.add(two)
+    egraph.add(a)
+    val ax2    = egraph.add(*(a, two))
+    val ax2_d2 = egraph.add(/(*(a, two), two))
+    val `2d2`  = egraph.add(/(two, two))
+    val ax_2d2 = egraph.add(*(a, /(two, two)))
+    val ax1    = egraph.add(*(a, one))
+    val as1    = egraph.add(<<(a, one))
+
+    egraph.merge(ax2, as1)
+    egraph.merge(ax2_d2, ax_2d2)
+    egraph.merge(`2d2`, one)
+    egraph.merge(ax1, a)
+
+
+    assert(egraph.idEq(one, `2d2`))
+    assert(egraph.idEq(ax2, as1))
+    assert(egraph.idEq(ax2_d2, ax_2d2))
+    assert(egraph.idEq(ax_2d2, ax1))
+    assert(egraph.idEq(ax_2d2, a))
+
+    assert(!egraph.idEq(ax2, ax2_d2))
+    assert(!egraph.idEq(ax2, `2d2`))
+    assert(!egraph.idEq(ax2, ax_2d2))
+    assert(!egraph.idEq(ax2, ax1))
+    assert(!egraph.idEq(ax2, a))
+    assert(!egraph.idEq(ax2_d2, `2d2`))
+
+  }
+
+  test("long chain congruence eGraph") {
+    val egraph = new EGraphTerms()
+    egraph.add(x)
+    val fx = egraph.add(F(x))
+    val ffx = egraph.add(F(fx))
+    val fffx = egraph.add(F(ffx))
+    val ffffx = egraph.add(F(fffx))
+    val fffffx = egraph.add(F(ffffx))
+    val ffffffx = egraph.add(F(fffffx))
+    val fffffffx = egraph.add(F(ffffffx))
+    val ffffffffx = egraph.add(F(fffffffx))
+
+
+    egraph.merge(ffffffffx, x)
+    egraph.merge(fffffx, x)
+    assert(egraph.idEq(fffx, x))
+    assert(egraph.idEq(ffx, x))
+    assert(egraph.idEq(fx, x))
+    assert(egraph.idEq(x, fx))
+
+  }
+
+/*
+
+  // Tests with Proofs
+  println("\n\n With Explains \n\n")
+
+  test("4 elements with congruence egraph test with explain") {
+    val egraph = new EGraphTerms()
+    val id1 = egraph.add(Term(a, List()))
+    val id2 = egraph.add(Term(b, List()))
+    val id3 = egraph.add(Term(F, List(id1)))
+    val id4 = egraph.add(Term(F, List(id2)))
+    egraph.merge(id1, id2)
+    assert(egraph.explain(id3, id4) == Some(List(egraph.Congruence((id3, id4)))) )
+  }
+
+  test("divide-mult-shift by 2 egraph test with explain") {
+
+    val egraph = new EGraphTerms()
+    val one = egraph.add(Term("one", List()))
+    val two = egraph.add(Term("two", List()))
+    val a = egraph.add(Term("a", List()))
+    val ax2 = egraph.add(Term("*", List(a, two)))
+    val ax2_d2 = egraph.add(Term("/", List(ax2, two)))
+    val `2d2` = egraph.add(Term("/", List(two, two)))
+    val ax_2d2 = egraph.add(Term("*", List(a, `2d2`)))
+    val ax1 = egraph.add(Term("*", List(a, one)))
+
+    val as1 = egraph.add(Term("<<", List(a, one)))
+    val * : TermSymbol = FunctionSymbol("*", 2)
+
+    egraph.merge(ax2, as1)
+    egraph.merge(ax2_d2, ax_2d2)
+    egraph.merge(`2d2`, one)
+    egraph.merge(ax1, a)
+
+    assert(egraph.explain(one, `2d2`) == Some(List(egraph.External((one, `2d2`)))) )
+    assert(egraph.explain(ax2, as1) == Some(List(egraph.External((ax2, as1)))) )
+    assert(egraph.explain(ax2_d2, ax_2d2) == Some(List(egraph.External((ax2_d2, ax_2d2)))) )
+
+    assert(egraph.explain(ax_2d2, ax1) == Some(List(egraph.Congruence((ax_2d2, ax1)))) )
+    assert(egraph.explain(ax_2d2, a) == Some(List(egraph.Congruence((ax_2d2, ax1)), egraph.External((ax1, a))) ))
+
+
+  }
+
+  test("long chain congruence eGraph with explain") {
+    val egraph = new EGraphTerms()
+    val x = egraph.add(Term("x", List()))
+    val fx = egraph.add(Term(F, List(x)))
+    val ffx = egraph.add(Term(F, List(fx)))
+    val fffx = egraph.add(Term(F, List(ffx)))
+    val ffffx = egraph.add(Term(F, List(fffx)))
+    val fffffx = egraph.add(Term(F, List(ffffx)))
+    val ffffffx = egraph.add(Term(F, List(fffffx)))
+    val fffffffx = egraph.add(Term(F, List(ffffffx)))
+    val ffffffffx = egraph.add(Term(F, List(fffffffx)))
+
+
+    egraph.merge(ffffffffx, x)
+    egraph.merge(fffffx, x)
+    assert(egraph.idEq(fffx, x))
+    assert(egraph.idEq(ffx, x))
+    assert(egraph.idEq(fx, x))
+    assert(egraph.idEq(x, fx))
+    assert(egraph.explain(fx, x) == Some(List(egraph.Congruence(fx, ffffffffx), egraph.External(ffffffffx, x))))
+
+  }
+}
+
+class EGraphTermTestWithProofs extends AnyFunSuite {
+  import Constants.*
+
+
+  println("\n\n With Proofs \n\n")
+
+
+  test("15 elements no congruence egraph test with redundant merges") {
+    val egraph = new EGraphTerms()
+    val id1 = egraph.add(Term(a, List()))
+    val id2 = egraph.add(Term(b, List()))
+    val id3 = egraph.add(Term(c, List()))
+    val id4 = egraph.add(Term(d, List()))
+    val id5 = egraph.add(Term(e, List()))
+    val id6 = egraph.add(Term(f, List()))
+    val id7 = egraph.add(Term(g, List()))
+    val id8 = egraph.add(Term(h, List()))
+    val id9 = egraph.add(Term(i, List()))
+    val id10 = egraph.add(Term(j, List()))
+    val id11 = egraph.add(Term(k, List()))
+    val id12 = egraph.add(Term(l, List()))
+    val id13 = egraph.add(Term(m, List()))
+    val id14 = egraph.add(Term(n, List()))
+    val id15 = egraph.add(Term(o, List()))
+    val base = Seq((id1, id3), (id5, id6), (id9, id11), (id13, id14), (id1, id2), (id15, id13), (id9, id13), (id7, id8), (id12, id11), (id2, id3), (id6, id5), (id15, id9), (id7, id5), (id9, id10))
+    egraph.merge(id1, id3)
+    egraph.merge(id5, id6)
+    egraph.merge(id9, id11)
+    egraph.merge(id13, id14)
+    egraph.merge(id1, id2)
+    egraph.merge(id15, id13)
+    egraph.merge(id9, id13)
+    egraph.merge(id7, id8)
+    egraph.merge(id12, id11)
+    egraph.merge(id2, id3)
+    egraph.merge(id6, id5)
+    egraph.merge(id15, id9)
+    egraph.merge(id7, id5)
+    egraph.merge(id9, id10)
+    assert(egraph.idEq(id2, id3))
+    assert(egraph.idEq(id6, id8))
+    assert(egraph.idEq(id9, id15))
+
+    println(s"\n Proving ${id2} === ${id3} \n")
+    println(egraph.prove(id2, id3, SC.Sequent(base.map(_ === _), List()), "s"))
+
+    println(s"\n Proving ${id6} === ${id8} \n")
+    println(egraph.prove(id6, id8, SC.Sequent(base.map(_ === _), List()), "s"))
+
+    println(s"\n Proving ${id9} === ${id15} \n")
+    println(egraph.prove(id9, id15, SC.Sequent(base.map(_ === _), List()), "s"))
+
+
+    println(s"\n Proving ${id15} === ${id9} \n")
+    println(egraph.prove(id15, id9, SC.Sequent(base.map(_ === _), List()), "s"))
+
+
+
+
+  }
+
+
+  test("4 elements with congruence egraph test with explain") {
+    val egraph = new EGraphTerms()
+    val id1 = egraph.add(Term(a, List()))
+    val id2 = egraph.add(Term(b, List()))
+    val id3 = egraph.add(Term(F, List(id1)))
+    val id4 = egraph.add(Term(F, List(id2)))
+    egraph.merge(id1, id2)
+    assert(egraph.explain(id3, id4) == Some(List(egraph.Congruence((id3, id4)))) )
+
+    println(s"\n Proving ${id3} === ${id4} \n")
+    println(egraph.explain(id3, id4))
+    println(egraph.prove(id3, id4, SC.Sequent(List(id1 === id2), List()), "s"))
+  }
+
+
+  test("divide-mult-shift by 2 egraph test with explain") {
+
+    val egraph = new EGraphTerms()
+    val one = egraph.add(Term("one", List()))
+    val two = egraph.add(Term("two", List()))
+    val a = egraph.add(Term("a", List()))
+    val ax2 = egraph.add(Term("*", List(a, two)))
+    val ax2_d2 = egraph.add(Term("/", List(ax2, two)))
+    val `2d2` = egraph.add(Term("/", List(two, two)))
+    val ax_2d2 = egraph.add(Term("*", List(a, `2d2`)))
+    val ax1 = egraph.add(Term("*", List(a, one)))
+
+    val as1 = egraph.add(Term("<<", List(a, one)))
+    val * : TermSymbol = FunctionSymbol("*", 2)
+
+    egraph.merge(ax2, as1)
+    egraph.merge(ax2_d2, ax_2d2)
+    egraph.merge(`2d2`, one)
+    egraph.merge(ax1, a)
+    val ctx = List(ax2 === as1, ax2_d2 === ax_2d2, `2d2` === one, ax1 === a)
+
+    assert(egraph.explain(one, `2d2`) == Some(List(egraph.External((one, `2d2`)))) )
+    assert(egraph.explain(ax2, as1) == Some(List(egraph.External((ax2, as1)))) )
+    assert(egraph.explain(ax2_d2, ax_2d2) == Some(List(egraph.External((ax2_d2, ax_2d2)))) )
+
+    assert(egraph.explain(ax_2d2, ax1) == Some(List(egraph.Congruence((ax_2d2, ax1)))) )
+    assert(egraph.explain(ax_2d2, a) == Some(List(egraph.Congruence((ax_2d2, ax1)), egraph.External((ax1, a))) ))
+
+    println(s"\n Proving ${one} === ${`2d2`} \n")
+    println(egraph.prove(one, `2d2`, SC.Sequent(ctx, List()), "s"))
+
+    println(s"\n Proving ${ax2} === ${as1} \n")
+    println(egraph.prove(ax2, as1, SC.Sequent(ctx, List()), "s"))
+
+    println(s"\n Proving ${ax2_d2} === ${ax_2d2} \n")
+    println(egraph.prove(ax2_d2, ax_2d2, SC.Sequent(ctx, List()), "s"))
+
+    println(s"\n Proving ${ax_2d2} === ${ax1} \n")
+    println(egraph.prove(ax_2d2, ax1, SC.Sequent(ctx, List()), "s"))
+
+    println(s"\n Proving ${ax_2d2} === ${a} \n")
+    println(egraph.prove(ax_2d2, a, SC.Sequent(ctx, List()), "s"))
+
+
+  }
+
+
+  test("long chain congruence eGraph with explain") {
+    val egraph = new EGraphTerms()
+    val x = egraph.add(Term("x", List()))
+    val fx = egraph.add(Term(F, List(x)))
+    val ffx = egraph.add(Term(F, List(fx)))
+    val fffx = egraph.add(Term(F, List(ffx)))
+    val ffffx = egraph.add(Term(F, List(fffx)))
+    val fffffx = egraph.add(Term(F, List(ffffx)))
+    val ffffffx = egraph.add(Term(F, List(fffffx)))
+    val fffffffx = egraph.add(Term(F, List(ffffffx)))
+    val ffffffffx = egraph.add(Term(F, List(fffffffx)))
+
+
+    egraph.merge(ffffffffx, x)
+    egraph.merge(fffffx, x)
+    assert(egraph.idEq(fffx, x))
+    assert(egraph.idEq(ffx, x))
+    assert(egraph.idEq(fx, x))
+    assert(egraph.idEq(x, fx))
+
+    val ctx = List(ffffffffx === x, fffffx === x)
+    def shorten(s:String): String = 
+      s.replace("F(F(F(F(F(F(F(F(x))))))))", "F8x")
+      .replace("F(F(F(F(F(F(F(x)))))))", "F7x")
+      .replace("F(F(F(F(F(F(x))))))", "F6x")
+      .replace("F(F(F(F(F(x)))))", "F5x")
+      .replace("F(F(F(F(x)))", "F4x")
+      .replace("F(F(F(x)))", "F3x")
+      .replace("F(F(x))", "F2x")
+      .replace("F(x)", "Fx")
+
+    println(s"\n Proving ${fffx} === ${x} \n")
+    println(shorten(egraph.prove(fffx, x, SC.Sequent(ctx, List()), "s").get.toString()))
+
+    println(s"\n Proving ${ffx} === ${x} \n")
+    println(shorten(egraph.prove(ffx, x, SC.Sequent(ctx, List()), "s").get.toString()))
+
+    println(s"\n Proving ${fx} === ${x} \n")
+    println(shorten(egraph.prove(fx, x, SC.Sequent(ctx, List()), "s").get.toString()))
+
+  }
+*/
+
+}
