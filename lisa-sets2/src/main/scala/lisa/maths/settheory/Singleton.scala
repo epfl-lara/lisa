24b9329731969ef681dc53c053a3eee895530867
Sankalp Gambhir <sankalp.gambhir42@gmail.com>
2024-11-04 20:51:33 +0100

    Save wip set theory library

---
+++ lisa-sets2/src/main/scala/lisa/maths/settheory/Singleton.scala
@@ -0,0 +1,30 @@
+package lisa.maths.settheory
+
+import lisa.maths.settheory.UnorderedPair.*
+import lisa.automation.Substitution
+
+object Singleton extends lisa.Main:
+
+  private val s = variable[Term]
+  private val x = variable[Term]
+  private val y = variable[Term]
+  private val z = variable[Term]
+  private val P = variable[Term >>: Formula]
+  private val Q = variable[Term >>: Term >>: Formula]
+
+  val singleton = DEF ( lambda(x, x <> x) )
+
+  extension (t: Term)
+    /**
+      * Prefix notation for singleton set
+      */
+    def unary_~ = singleton(x)
+
+  show(singleton.definition)
+
+  val membership = Theorem( x ∈ ~x ):
+    have(x ∈ (x <> x)) by Restate.from(UnorderedPair.firstMember of (y := x)) 
+    thenHave(thesis) by Substitution.Apply(singleton.definition)
+
+
+end Singleton
