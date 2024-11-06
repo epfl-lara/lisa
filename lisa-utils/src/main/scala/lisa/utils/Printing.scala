73e0818b79f3db7f82c648af79d6b6c8c9dcffec
Sankalp Gambhir <sankalp.gambhir42@gmail.com>
2024-11-06 15:02:09 +0100

    Change printList separator to prefix

--- lisa-utils/src/main/scala/lisa/utils/Printing.scala
+++ lisa-utils/src/main/scala/lisa/utils/Printing.scala
@@ -1,6 +1,6 @@
 package lisa.utils
 
 object Printing:
-  def printList[T](seq: Iterable[T], separator: String = "\n\t"): String =
-    (seq lazyZip (0 until seq.size)).map((x, i) => s"$i: $x").mkString(separator)
+  def printList[T](seq: Iterable[T], prefix: String = "\n\t"): String =
+    prefix + (seq lazyZip (0 until seq.size)).map((x, i) => s"$i: $x").mkString(prefix)
 
