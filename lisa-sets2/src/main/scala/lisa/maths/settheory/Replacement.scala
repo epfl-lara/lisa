26304eda5eab2fe9bdc7772e0e14cf78afcfe05a
Sankalp Gambhir <sankalp.gambhir42@gmail.com>
2024-11-08 12:26:43 +0100

    Correct replacement proof

--- lisa-sets2/src/main/scala/lisa/maths/settheory/Replacement.scala
+++ lisa-sets2/src/main/scala/lisa/maths/settheory/Replacement.scala
@@ -11,10 +11,11 @@ object Replacement extends lisa.Main:
   val t = variable[Term]
   val s = variable[Term]
   val A = variable[Term]
+  val B = variable[Term]
   val f = variable[Term >>: Term]
   val P = variable[Term >>: Term >>: Formula]
 
-  val map = DEF(lambda(t, lambda(f, ε(s, ∀(x, (x ∈ s) <=> ∃(y, y ∈ t /\ (y === f(x))))))))
+  val map = DEF(lambda(t, lambda(f, ε(s, ∀(y, (y ∈ s) <=> ∃(x, x ∈ t /\ (y === f(x))))))))
 
   private val replacement: map.type = map
 
@@ -26,9 +27,9 @@ object Replacement extends lisa.Main:
    * The existence of the image of a set under a function. Or, the functional
    * form of the replacement schema.
    */
-  val existence = Theorem(∃(s, ∀(x, (x ∈ s) <=> ∃(y, y ∈ t /\ (y === f(x)))))):
+  val existence = Theorem(∃(s, ∀(y, (y ∈ s) <=> ∃(x, x ∈ t /\ (y === f(x)))))):
     val inst = replacementSchema of (A := t, P := lambda(x, lambda(y, y === f(x))))
-    val conditional = have(∀(x, x ∈ t ==> ∀(y, ∀(z, ((y === f(x)) /\ (z === f(x))) ==> (y === z)))) |- ∃(s, ∀(x, (x ∈ s) <=> ∃(y, y ∈ t /\ (y === f(x)))))) by Weakening(inst)
+    val conditional = have(∀(x, x ∈ t ==> ∀(y, ∀(z, ((y === f(x)) /\ (z === f(x))) ==> (y === z)))) |- ∃(s, ∀(y, (y ∈ s) <=> ∃(x, x ∈ t /\ (y === f(x)))))) by Weakening(inst)
 
     val eqTautology =
       have(((y === f(x)) /\ (z === f(x))) ==> (y === z)) by Weakening(Equality.transitivity of (x := y, y := f(x), z := z))
@@ -44,12 +45,12 @@ object Replacement extends lisa.Main:
    *
    * `∀(x, x ∈ s.map(f) <=> ∃(y, y ∈ s /\ x === f(y)))`
    */
-  val definition: THM = Theorem(∀(x, x ∈ s.map(f) <=> ∃(y, y ∈ s /\ (x === f(y))))):
-    have(∀(x, x ∈ y <=> ∃(y, y ∈ s /\ (x === f(y)))) |- ∀(x, x ∈ y <=> ∃(y, y ∈ s /\ (x === f(y))))) by Hypothesis
-    thenHave(∀(x, x ∈ y <=> ∃(y, y ∈ s /\ (x === f(y)))) |- ∀(x, x ∈ ε(t, ∀(x, x ∈ t <=> ∃(y, y ∈ s /\ (x === f(y))))) <=> ∃(y, y ∈ s /\ (x === f(y))))) by RightEpsilon
-    thenHave(∀(x, x ∈ y <=> ∃(y, y ∈ s /\ (x === f(y)))) |- ∀(x, x ∈ s.map(f) <=> ∃(y, y ∈ s /\ (x === f(y))))) by Substitution.Apply(map.definition)
-    thenHave(∃(y, ∀(x, x ∈ y <=> ∃(y, y ∈ s /\ (x === f(y))))) |- ∀(x, x ∈ s.map(f) <=> ∃(y, y ∈ s /\ (x === f(y))))) by LeftExists
-    have(thesis) by Cut(existence, lastStep)
+  val definition: THM = Theorem(∀(y, y ∈ s.map(f) <=> ∃(x, x ∈ s /\ (y === f(x))))):
+    have(∀(y, y ∈ t <=> ∃(x, x ∈ s /\ (y === f(x)))) |-     ∀(y, y ∈ t                                                 <=> ∃(x, x ∈ s /\ (y === f(x))))) by Hypothesis
+    thenHave(∀(y, y ∈ t <=> ∃(x, x ∈ s /\ (y === f(x)))) |- ∀(y, y ∈ ε(t, ∀(y, y ∈ t <=> ∃(x, x ∈ s /\ (y === f(x))))) <=> ∃(x, x ∈ s /\ (y === f(x))))) by RightEpsilon.withParameters(∀(y, y ∈ t <=> ∃(x, x ∈ s /\ (y === f(x)))), t, t)
+    thenHave(∀(y, y ∈ t <=> ∃(x, x ∈ s /\ (y === f(x)))) |- ∀(y, y ∈ s.map(f) <=> ∃(x, x ∈ s /\ (y === f(x))))) by Substitution.Apply(map.definition of (t := s))
+    thenHave(∃(t, ∀(y, y ∈ t <=> ∃(x, x ∈ s /\ (y === f(x))))) |- ∀(y, y ∈ s.map(f) <=> ∃(x, x ∈ s /\ (y === f(x))))) by LeftExists
+    have(thesis) by Cut(existence of (t := s), lastStep)
 
   /**
    * The replacement property of a [[map]]ped set.
