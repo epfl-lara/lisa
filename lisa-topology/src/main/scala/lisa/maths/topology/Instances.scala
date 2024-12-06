30456d569f45e24a47f4f8f3334c5791eb94a3ef
Dobrin Bashev <dobitoto6@gmail.com>
2024-12-06 17:17:29 +0100

    Add implicationProof

--- lisa-topology/src/main/scala/lisa/maths/topology/Instances.scala
+++ lisa-topology/src/main/scala/lisa/maths/topology/Instances.scala
@@ -351,7 +351,67 @@ object Instances extends lisa.Main {
   val singletonSetsUniquenes = Theorem(
     ∃!(z, ∀(t, in(t, z) <=> exists(x, in(x, S) /\ (t === singleton(x)))))
   ) {
-    val implicationProof = have(exists(x, in(x, S) /\ (t === singleton(x))) ==> in(t, union(cartesianProduct(S, S)))) subproof { sorry }
+    val implicationProof = have(exists(x, in(x, S) /\ (t === singleton(x))) ==> in(t, union(cartesianProduct(S, S)))) subproof { 
+
+      val elementInSingleton = have(in(x, singleton(x))) subproof { 
+        have(thesis) by Tautology.from(pairAxiom of (x := x, y := x, z := x))
+      }
+
+      val singletonInPowerSet = have(in(x, S) |- in(singleton(x), powerSet(S))) subproof {
+        assume(in(x, S))
+        val introduceY = thenHave((x === y) |- in(y, S)) by Substitution.ApplyRules(x === y)
+        val useSingleton = have(in(y, singleton(x)) |- in(y, S)) by Tautology.from(introduceY, singletonHasNoExtraElements)
+        thenHave(() |- (!(in(y, singleton(x))), in(y, S))) by RightNot
+        thenHave(() |- in(y, singleton(x)) ==> in(y, S)) by Tautology
+        val universalY = thenHave(() |- forall(y, in(y, singleton(x)) ==> in(y, S))) by RightForall
+        val singletonIsSubset = have(() |- singleton(x) ⊆ S) by Tautology.from(universalY, subsetAxiom of (z := y, y := S, x := singleton(x)))
+        have(thesis) by Tautology.from(singletonIsSubset, powerAxiom of (x := singleton(x), y := S))
+      }
+
+      val abExist = have((singleton(t) === pair(x, x)) /\ in(x, S) |- ∃(a, ∃(b, (singleton(t) === pair(a, b)) /\ in(a, S) /\ in(b, S)))) subproof {
+        have((singleton(t) === pair(x, x)) /\ in(x, S) |- (singleton(t) === pair(x, x)) /\ in(x, S) /\ in(x, S)) by Restate 
+        thenHave((singleton(t) === pair(x, x)) /\ in(x, S) |- exists(b, (singleton(t) === pair(x, b)) /\ in(x, S) /\ in(b, S))) by RightExists
+        thenHave((singleton(t) === pair(x, x)) /\ in(x, S) |- exists(a, exists(b, (singleton(t) === pair(a, b)) /\ in(a, S) /\ in(b, S)))) by RightExists
+      }
+
+      val cartesianInstantiated = have(in(singleton(t), cartesianProduct(S, S)) <=> (in(singleton(t), powerSet(powerSet(setUnion(S, S)))) 
+      /\ ∃(a, ∃(b, (singleton(t) === pair(a, b)) /\ in(a, S) /\ in(b, S))))) subproof {
+        val instance = have((cartesianProduct(x, y) === cartesianProduct(x, y)) <=> ∀(t, in(t, cartesianProduct(x, y)) <=> (in(t, powerSet(powerSet(setUnion(x, y)))) 
+        /\ ∃(a, ∃(b, (t === pair(a, b)) /\ in(a, x) /\ in(b, y)))))) by InstantiateForall(cartesianProduct(x, y))(cartesianProduct.definition)
+        thenHave(∀(t, in(t, cartesianProduct(x, y)) <=> (in(t, powerSet(powerSet(setUnion(x, y)))) 
+        /\ ∃(a, ∃(b, (t === pair(a, b)) /\ in(a, x) /\ in(b, y)))))) by Tautology
+        thenHave(in(singleton(t), cartesianProduct(x, y)) <=> (in(singleton(t), powerSet(powerSet(setUnion(x, y)))) 
+        /\ ∃(a, ∃(b, (singleton(t) === pair(a, b)) /\ in(a, x) /\ in(b, y))))) by InstantiateForall(singleton(t))
+        thenHave(forall(x, in(singleton(t), cartesianProduct(x, y)) <=> (in(singleton(t), powerSet(powerSet(setUnion(x, y)))) 
+        /\ ∃(a, ∃(b, (singleton(t) === pair(a, b)) /\ in(a, x) /\ in(b, y)))))) by RightForall
+        thenHave(in(singleton(t), cartesianProduct(S, y)) <=> (in(singleton(t), powerSet(powerSet(setUnion(S, y)))) 
+        /\ ∃(a, ∃(b, (singleton(t) === pair(a, b)) /\ in(a, S) /\ in(b, y))))) by InstantiateForall(S)
+        thenHave(forall(y, in(singleton(t), cartesianProduct(S, y)) <=> (in(singleton(t), powerSet(powerSet(setUnion(S, y)))) 
+        /\ ∃(a, ∃(b, (singleton(t) === pair(a, b)) /\ in(a, S) /\ in(b, y)))))) by RightForall
+        thenHave(in(singleton(t), cartesianProduct(S, S)) <=> (in(singleton(t), powerSet(powerSet(setUnion(S, S)))) 
+        /\ ∃(a, ∃(b, (singleton(t) === pair(a, b)) /\ in(a, S) /\ in(b, S))))) by InstantiateForall(S)
+      }
+
+      val init = have(in(x, S) |- in(x, S)) by Restate
+      val inSS = have(in(x, S) |- in(x, setUnion(S, S))) by Tautology.from(init, setUnionMembership of (z := x, x := S, y := S))
+      val step1 = have(in(x, S) |- in(singleton(x), powerSet(setUnion(S, S))) /\ (singleton(singleton(x)) === pair(x, x))) by 
+        Tautology.from(inSS, singletonInPowerSet of (S := setUnion(S, S)))
+      thenHave((in(x, S), t === singleton(x)) |- in(t, powerSet(setUnion(S, S))) /\ (singleton(t) === pair(x, x))) by 
+        Substitution.ApplyRules(singleton(x) === t)
+      val introduceT = thenHave(in(x, S) /\ (t === singleton(x)) |- in(t, powerSet(setUnion(S, S))) /\ (singleton(t) === pair(x, x))) by Tautology
+      val step2 = have(in(x, S) /\ (t === singleton(x)) |- in(singleton(t), powerSet(powerSet(setUnion(S, S)))) /\ (singleton(t) === pair(x, x))) by 
+        Tautology.from(introduceT, singletonInPowerSet of (x := t, S := powerSet(setUnion(S, S))))
+      val extendRHS = have(in(x, S) /\ (t === singleton(x)) |- in(singleton(t), powerSet(powerSet(setUnion(S, S)))) /\ in(t, singleton(t))
+        /\ ((singleton(t) === pair(x, x)) /\ in(x, S))) by Tautology.from(step2, elementInSingleton of (x := t))
+      val existAB = have(in(x, S) /\ (t === singleton(x)) |- in(singleton(t), powerSet(powerSet(setUnion(S, S)))) /\ in(t, singleton(t))
+        /\ ∃(a, ∃(b, (singleton(t) === pair(a, b)) /\ in(a, S) /\ in(b, S)))) by Tautology.from(extendRHS, abExist)
+      val cartesian = have(in(x, S) /\ (t === singleton(x)) |- in(singleton(t), cartesianProduct(S, S)) /\ in(t, singleton(t))) by 
+        Tautology.from(existAB, cartesianInstantiated)
+      val introduceZ = thenHave(in(x, S) /\ (t === singleton(x)) |- exists(z, in(z, cartesianProduct(S, S)) /\ in(t, z))) by RightExists
+      val unionOfProduct = have((in(x, S) /\ (t === singleton(x))) |- in(t, union(cartesianProduct(S, S)))) by 
+        Tautology.from(introduceZ, unionAxiom of (y := z, x := cartesianProduct(S, S), z := t))
+      thenHave(exists(x, (in(x, S) /\ (t === singleton(x)))) |- in(t, union(cartesianProduct(S, S)))) by LeftExists
+    }
     have(() |- existsOne(z, forall(t, in(t, z) <=> exists(x, in(x, S) /\ (t === singleton(x)))))) by UniqueComprehension.fromOriginalSet(
       union(cartesianProduct(S, S)),
       lambda(t, exists(x, in(x, S) /\ (t === singleton(x)))),
