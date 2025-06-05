cc8500fd8c3428d53fbfa143a426c53b2575fe0e
Dario Halilovic <dario.halilovic@epfl.ch>
2025-06-05 15:26:56 +0200

    Port Extensionality tactic

--- lisa-sets2/src/main/scala/lisa/maths/settheory/Extensionality.scala
+++ lisa-sets2/src/main/scala/lisa/maths/settheory/Extensionality.scala
@@ -1,49 +1,48 @@
 package lisa.maths.settheory
 
+import lisa.utils.prooflib.BasicStepTactic.*
+import lisa.utils.prooflib.SimpleDeducedSteps.*
 import lisa.utils.prooflib.ProofTacticLib.ProofTactic
-import lisa.utils.prooflib.Library
+import lisa.automation.Tautology
 import lisa.SetTheoryLibrary
-import lisa.utils.prooflib.ProofTacticLib.ProofFactSequentTactic
+import lisa.utils.fol.FOL.{*, given}
 
-object Extensionality extends lisa.Main:
+val lib = SetTheoryLibrary
+val x = variable[Ind]
+val y = variable[Ind]
 
-  private val s = variable[Ind]
-  private val x = variable[Ind]
-  private val y = variable[Ind]
-  private val z = variable[Ind]
-  private val P = variable[Ind >>: Prop]
-  private val Q = variable[Ind >>: Ind >>: Prop]
+import lib.{*, given}
 
-  val implied = Theorem(forall(z, z ∈ x <=> z ∈ y) |- (x === y)):
-    have(thesis) by Weakening(extensionalityAxiom)
+/**
+ * Given that z ∈ x <=> z ∈ y, prove that x = y if z is free.
+ *
+ *  Γ ⊢ z ∈ x <=> z ∈ y, Δ
+ * ------------------------   z does not appear in Γ
+ *      Γ ⊢ x === y, Δ
+ */
 
-  /**
-   * Given that z ∈ x <=> z ∈ y, prove that x = y if z is free.
-   *
-   *  Γ ⊢ z ∈ x <=> z ∈ y, Δ
-   * ------------------------ z not in Γ
-   *      Γ ⊢ x === y, Δ
-   */
-  def tactic(using proof: Proof)(premiseStep: proof.Fact)(conclusion: Sequent) =
-    val premise = proof.getSequent(premiseStep)
-    val boundVars = premise.left.flatMap(_.freeVars)
-    inline def valid(z1: Variable[Ind], z2: Variable[Ind], x: Expr[Ind], y: Expr[Ind]) =
-      z1 == z2 && !boundVars.contains(z1) && conclusion.right.exists(isSame(_, x === y))
-    val pivot: Option[(Variable[Ind], Expr[Ind], Expr[Ind])] = premise.right.collectFirst:
-      case (<=> #@ (∈ #@ (z1: Variable[Ind]) #@ (x: Expr[Ind])) #@ (∈ #@ (z2: Variable[Ind]) #@ (y: Expr[Ind]))) if valid(z1, z2, x, y) => (z1, x, y)
+def Extensionality(using proof: SetTheoryLibrary.Proof)(premise: proof.Fact)(conclusion: Sequent): proof.ProofTacticJudgement = {
+  val premiseSeq = premise.statement
+  val boundVars = premiseSeq.left.flatMap(_.freeVars)
 
-    pivot match
-      case None =>
-        proof.InvalidProofTactic("Could not find a formula of the form z ∈ x <=> z ∈ y in the RHS of the premise.")
-      case Some((z, xe, ye)) =>
-        TacticSubproof:
-          val pivot = z ∈ xe <=> z ∈ ye
-          val qpivot = forall(z, pivot)
-          val eq = xe === ye
-          val baseSequent = premise ->> pivot
-          val implication = proof.InstantiatedFact(implied, Seq(x := xe, y := ye))
+  inline def valid(z1: Variable[Ind], z2: Variable[Ind], x: Expr[Ind], y: Expr[Ind]) =
+    z1 == z2 && !boundVars.contains(z1) && conclusion.right.exists(isSame(_, x === y))
 
-          have(baseSequent +>> qpivot) by RightForall.withParameters(pivot, z)(premiseStep)
-          have(baseSequent +>> eq) by Cut.withParameters(qpivot)(lastStep, implication)
+  val pivot: Option[(Variable[Ind], Expr[Ind], Expr[Ind])] = premiseSeq.right.collectFirst {
+    case ((z: Variable[Ind]) ∈ x) <=> ((z_ : Variable[Ind]) ∈ y) if valid(z, z_, x, y) => (z, x, y)
+  }
 
-end Extensionality
+  pivot match {
+    case None =>
+      proof.InvalidProofTactic("Could not find a formula of the form z ∈ x <=> z ∈ y in the RHS of the premise.")
+    case Some((z, x_, y_)) =>
+      TacticSubproof {
+        val equiv = z ∈ x_ <=> z ∈ y_
+        val eq = x_ === y_
+        val baseSequent = premiseSeq ->> equiv
+
+        have(baseSequent +>> ∀(z, equiv)) by RightForall.withParameters(equiv, z)(premise)
+        thenHave(baseSequent +>> eq) by Tautology.fromLastStep(lib.extensionalityAxiom of (x := x_, y := y_))
+      }
+  }
+}
