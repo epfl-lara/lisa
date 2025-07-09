df282215c33f46cb76489d133474e512d4d6d8be
Simon Guilloud <simon.guilloud@bluewin.ch>
2025-07-09 10:21:14 +0200

    add kernel check before printing "valid".

--- lisa-sets/src/main/scala/lisa/tptp/TPTP_Lisa.scala
+++ lisa-sets/src/main/scala/lisa/tptp/TPTP_Lisa.scala
@@ -40,17 +40,25 @@ object TPTP_Lisa {
     val optproof2 = Tautology.solveSequent(sequent)
     optproof match {
       case Some(proof) =>
-        println("SZS valid")
-        val tptp_proof = proofToTPTP(proof, Map(), ("sequent_conjecture", sequent), true)
-        val endproof = tptp_proof :+ {
-          val prob_conj = leo.datastructures.TPTP.FOF.Logical(formulaToFOFFormula(problem.conjecture.toFormula.formula, Set(), true))
-          val axioms_names = problem.axioms.map(_.name)
-          leo.datastructures.TPTP.FOFAnnotated("final", "plain", prob_conj, premisesToAnnotationsStr(axioms_names, "big_cut"))
+        val judgement = checkSCProof(proof)
+        if (judgement.isValid) {
+          println("% SZS status Theorem")
+          val tptp_proof = proofToTPTP(proof, Map(), ("sequent_conjecture", sequent), true)
+          val endproof = tptp_proof :+ {
+            val prob_conj = leo.datastructures.TPTP.FOF.Logical(formulaToFOFFormula(problem.conjecture.toFormula.formula, Set(), true))
+            val axioms_names = problem.axioms.map(_.name)
+            leo.datastructures.TPTP.FOFAnnotated("final", "plain", prob_conj, premisesToAnnotationsStr(axioms_names, "big_cut"))
+          }
+          println("TPTP proof:")
+          endproof.foreach { p => println(p.pretty) }
+        } else {
+          println("% SZS status Error")
+          println("A proof was found but rejected by the kernel checker.")
+          println(prettySCProof(judgement))
         }
-        println("TPTP proof:")
-        endproof.foreach { p => println(p.pretty) }
+        
       case None =>
-          println("%Cannot prove " + sequent.repr)
+          println("Cannot prove " + sequent.repr)
           println("% SZS status GaveUp")
         
     }
