2e9c77afe0b8c60d523d4fb434987fad332c8869
Sankalp Gambhir <sankalp.gambhir42@gmail.com>
2026-06-23 22:30:53 +0200

    Add reporting helpers

--- lisa-utilcfs/src/main/scala/lisa/utilcfs/prooflib/BasicStep.scala
+++ lisa-utilcfs/src/main/scala/lisa/utilcfs/prooflib/BasicStep.scala
@@ -1,11 +1,16 @@
 package lisa.utilcfs.prooflib
 
+import lisa.kernelcf.fol.{FOL => KF}
 import lisa.kernelcf.{proof => K}
-import lisa.utilcfs.fol.FOL.*
 import lisa.utilcfs.collection.Extensions.{*, given}
+import lisa.utilcfs.fol.FOL.*
 import lisa.utilcfs.prooflib.Helpers.*
 
 object BasicStep:
+
+  ///////////////////////////////////////////////////////////////////////////////
+  // Error reporting helpers
+  ///////////////////////////////////////////////////////////////////////////////
 
   case class FrontKernelMismatch(reason: String) extends Exception:
     def msg = s"[FATAL] Front-end and kernel mismatch: $reason"
@@ -21,6 +26,43 @@ object BasicStep:
 
   private def theoryMismatch(file: sourcecode.File, line: sourcecode.Line)(step: String, expected: K.Theory, actual: K.Theory): ProofError =
     SoftError(withParams(s"In step $step, premises and conclusion had mismatched underlying theories.", "Expected" -> expected, "Actual" -> actual), file, line)
+
+  private def liftGeneralError(file: sourcecode.File, line: sourcecode.Line)(step: String, err: K.GeneralError): ProofError =
+    err match
+      case e: K.TheoryMismatch => theoryMismatch(file, line)(step, e.expected, e.actual)
+      case e: K.SortMismatch =>
+        SoftError(
+          withParams(s"In step $step, an expression has the wrong sort.", "Expected" -> e.expected, "Actual" -> e.actual, "Expression" -> e.expression),
+          file,
+          line
+        )
+
+  private def inferenceFailure(file: sourcecode.File, line: sourcecode.Line)(message: String, conclusion: Sequent, params: (String, Any)*)(using Library): ProofJudgement =
+    ProofCarrier(
+      Set(SoftError(withParams(message, ("Conclusion", conclusion) +: params*), file, line)),
+      conclusion.underlying,
+      None,
+      ()
+    )
+
+  ///////////////////////////////////////////////////////////////////////////////
+  // Proof construction helpers
+  ///////////////////////////////////////////////////////////////////////////////
+
+  private def successful(thm: K.Thm)(using Library): ProofJudgement =
+    ProofCarrier(Set.empty, thm.statement, Some(thm), ())
+
+  ///////////////////////////////////////////////////////////////////////////////
+  // Basic Step Definitions
+  //
+  // Each step defines an apply function to be used in proof scripts, ideally
+  // performing parameter inference for convenience. In the case it is inferring
+  // some parameters, the actual kernel call should be delegated to a version of
+  // the step with explicit parameters, called `withParameters` by convention.
+  //
+  // Basic steps should largely just be wrappers around kernel calls with some
+  // parameter inference.
+  ///////////////////////////////////////////////////////////////////////////////
 
   object Sorry:
     private def liftError(err: K.Sorry.ErrorType): ProofError = 
@@ -44,7 +86,7 @@ object BasicStep:
         .mapLeft(liftError)
         .lift(underlying)
 
-  object Hypothesis:
+  object Hypothesis extends SequentTactic:
     private def liftError(file: sourcecode.File, line: sourcecode.Line)(conclusion: Sequent, pivot: Expr[Prop])(err: K.Hypothesis.ErrorType): ProofError = 
       err match
         // step-specific errors
