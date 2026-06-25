9c30780ab2d8429778a07eced2e7bcc4431bf98b
Sankalp Gambhir <sankalp.gambhir42@gmail.com>
2026-06-25 09:13:31 +0200

    Document proof carrier functions

--- lisa-utilcfs/src/main/scala/lisa/utilcfs/prooflib/ProofCarrier.scala
+++ lisa-utilcfs/src/main/scala/lisa/utilcfs/prooflib/ProofCarrier.scala
@@ -11,12 +11,27 @@ final case class ProofCarrier[+T](
 )(using lib: Library):
   given K.Theory = lib.theory
 
+  /**
+    * A carrier is valid iff it has no accumulated errors and has a valid
+    * justification.
+    */
   def isValid: Boolean = errors.isEmpty && justification.nonEmpty
+  /**
+   * Whether this carrier has a valid justification. A carrier may have errors
+   * but still have a valid justification.
+   */
   def hasJustification: Boolean = justification.nonEmpty
 
+  /**
+    * This carrier with the payload transformed by the given function.
+    */
   def map[U](f: T => U): ProofCarrier[U] =
     copy(payload = f(payload))
 
+  /**
+    * This carrier with the payload and justification transformed by the given
+    * function.
+    */
   def flatMap[U](f: (T, Thm) => ProofCarrier[U]): ProofCarrier[U] =
     val just = justification.getOrElse:
       K.Sorry(statement) match
@@ -24,12 +39,31 @@ final case class ProofCarrier[+T](
     val next = f(payload, just)
     next.copy(errors = errors ++ next.errors)
 
+  /**
+    * This carrier with an additional error.
+    */
   def withError(error: ProofError): ProofCarrier[T] =
     copy(errors = errors + error)
 
+  /**
+    * This carrier with additional appended errors.
+    */
   def withErrors(extraErrors: Iterable[ProofError]): ProofCarrier[T] =
     copy(errors = errors ++ extraErrors)
 
+  /**
+    * This carrier with an overriden justification. Used to add a step while
+    * preserving values and errors.
+    *
+    * Use [[flatMap]] to additionally transform the existing justification
+    * and/or payload.
+    */
+  def withJustification(just: Thm): ProofCarrier[T] =
+    copy(justification = Some(just))
+
+  /**
+    * This carrier with the payload discarded.
+    */
   def judgement: ProofJudgement =
     copy(payload = ())
 
