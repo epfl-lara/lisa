aa9df81c854bd6770d45cb3a1ddedc4a02f83cc9
Sankalp Gambhir <sankalp.gambhir42@gmail.com>
2026-07-02 08:23:42 +0200

    Add theorem accessors

--- lisa-utilcfs/src/main/scala/lisa/utilcfs/prooflib/Library.scala
+++ lisa-utilcfs/src/main/scala/lisa/utilcfs/prooflib/Library.scala
@@ -1,7 +1,54 @@
 package lisa.utilcfs.prooflib
 
 import lisa.kernelcf.proof.{Sequent, Theory}
+import lisa.utilcfs.K
+
+import scala.collection.mutable
+import scala.collection.View
 
 abstract class Library:
   val theory: Theory = Theory.empty
   given Theory = theory
+
+  private val theoremByFullName = mutable.LinkedHashMap.empty[String, Theorem]
+  private val theoremByShortName = mutable.HashMap.empty[String, Vector[Theorem]]
+
+  /**
+   * Provides access to theorems in the library.
+   */
+  object theorems:
+    /**
+     * Mutably update the named theorem registry
+     */
+    private[prooflib] def register(theorem: Theorem): Unit =
+      require(!theoremByFullName.contains(theorem.fullName), s"Theorem ${theorem.fullName} is already registered.")
+      theoremByFullName.update(theorem.fullName, theorem)
+      theoremByShortName.updateWith(theorem.shortName):
+        case Some(existing) => Some(existing :+ theorem)
+        case None => Some(Vector(theorem))
+
+    /**
+      * A view over all named registered theorems.
+      */
+    def all: View[Theorem] =
+      theoremByFullName.values.view
+
+    /**
+     * Lookup a theorem by full or short name (in that order of preference).
+     */
+    def get(name: String): Option[Theorem] =
+      getFull(name).orElse(getShort(name))
+
+    /**
+      * Lookup a theorem by full name.
+      */
+    def getFull(fullName: String): Option[Theorem] =
+      theoremByFullName.get(fullName)
+
+    /**
+     * Lookup a theorem by short name, if the short name is unambiguous.
+     */
+    def getShort(shortName: String): Option[Theorem] =
+      theoremByShortName.get(shortName) match
+        case Some(Vector(single)) => Some(single)
+        case _ => None
