cd82403933ed3d77b8c13c59a12f2c5e248b1189
Sankalp Gambhir <sankalp.gambhir42@gmail.com>
2026-07-16 11:48:24 +0200

    Move front theorem under FOL

--- lisa-utilcfs/src/main/scala/lisa/utilcfs/prooflib/Thm.scala
+++ lisa-utilcfs/src/main/scala/lisa/utilcfs/prooflib/Thm.scala
@@ -1,20 +1,6 @@
 package lisa.utilcfs.prooflib
 
-import lisa.utilcfs.K
-import lisa.utilcfs.fol.FOL.*
+import lisa.utilcfs.fol.FOL
 
-final case class Thm(statement: Sequent, kernel: K.Thm):
-  def kernelStatement: K.Sequent = kernel.statement
-  def left: Set[Expr[Prop]] = statement.left
-  def right: Set[Expr[Prop]] = statement.right
-  def leftK: Set[K.Expression] = kernel.statement.left
-  def rightK: Set[K.Expression] = kernel.statement.right
-
-object Thm:
-  def apply(kernel: K.Thm): Thm =
-    Thm(Sequent(kernel.statement.left.map(liftFormula), kernel.statement.right.map(liftFormula)), kernel)
-
-  def liftFormula(expression: K.Expression): Expr[Prop] =
-    liftExpression(expression).asInstanceOf[Expr[Prop]]
-
-  given asKernel: Conversion[Thm, K.Thm] = _.kernel
+type Thm = FOL.Thm
+val Thm: FOL.Thm.type = FOL.Thm
